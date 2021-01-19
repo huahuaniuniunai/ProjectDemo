7824e188811cc22e45807d4f667aa13c2658a80b
SmallPineapple <491899136@qq.com>
2021-01-19 17:12:45 +0800

    添加悬浮按钮

--- app/src/main/java/com/example/projectdemo/pages/MessageFragment.java
+++ app/src/main/java/com/example/projectdemo/pages/MessageFragment.java
@@ -10,15 +10,38 @@ import androidx.annotation.Nullable;
 
 import com.example.projectdemo.R;
 import com.example.projectdemo.mvp.BaseFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.gyf.immersionbar.ImmersionBar;
 import com.gyf.immersionbar.components.ImmersionFragment;
 
-public class MessageFragment extends BaseFragment {
+public class MessageFragment extends BaseFragment implements View.OnClickListener {
     private View view;
+    private FloatingActionButton fab;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_message, container, false);
+        initView();
+        initEvent();
         return view;
+    }
+
+    private void initView() {
+        fab = view.findViewById(R.id.message_add);
+    }
+
+    private void initEvent() {
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.message_add:
+                toast("欢迎光临！");
+                break;
+            default:
+                break;
+        }
     }
 }
