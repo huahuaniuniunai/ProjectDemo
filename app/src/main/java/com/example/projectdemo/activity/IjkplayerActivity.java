e6d9f7081a2e945823ad52621d87b61cd95ea631
SmallPineapple <491899136@qq.com>
2021-02-02 14:38:42 +0800

    直播布局优化

--- app/src/main/java/com/example/projectdemo/activity/IjkplayerActivity.java
+++ app/src/main/java/com/example/projectdemo/activity/IjkplayerActivity.java
@@ -1,20 +1,29 @@
 package com.example.projectdemo.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.projectdemo.R;
+import com.example.projectdemo.mvp.BaseActivity;
+import com.gyf.immersionbar.ImmersionBar;
 
-public class IjkplayerActivity extends AppCompatActivity {
+public class IjkplayerActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ijkplayer);
 
+    }
+
+    @Override
+    protected void setBar() {
+        ImmersionBar.with(this)
+                .titleBar(R.id.tb_login)
+                .keyboardEnable(true)
+                .navigationBarColor(R.color.page)
+                .init();
     }
 
     public static void actionStart(Context context) {
