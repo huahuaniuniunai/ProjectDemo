1af5545c8497c5b7bf0f0ac7af83009da4a576bd
SmallPineapple <491899136@qq.com>
2021-02-24 14:36:16 +0800

    1、批量动态申请权限 2、新增vitamio第三方播放器控件

--- app/src/main/java/com/example/projectdemo/activity/vitamio/VitamioActivity.java
+++ app/src/main/java/com/example/projectdemo/activity/vitamio/VitamioActivity.java
@@ -1,19 +1,82 @@
 package com.example.projectdemo.activity.vitamio;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import io.vov.vitamio.Vitamio;
+import io.vov.vitamio.widget.VideoView;
 
 import com.example.projectdemo.R;
 import com.example.projectdemo.mvp.BaseActivity;
 import com.gyf.immersionbar.ImmersionBar;
+import com.lcodecore.tkrefreshlayout.utils.DensityUtil;
 
-public class VitamioActivity extends BaseActivity {
+import io.vov.vitamio.MediaPlayer;
+import io.vov.vitamio.widget.MediaController;
+
+import android.content.pm.ActivityInfo;
+import android.graphics.Rect;
+import android.util.DisplayMetrics;
+import android.view.KeyEvent;
+import android.widget.LinearLayout;
+
+
+
+public class VitamioActivity extends BaseActivity implements View.OnClickListener, MediaPlayer.OnInfoListener,
+        MediaPlayer.OnBufferingUpdateListener {
+
+    private Button bt_vit1;
+    private Button bt_vit2;
+    private Button bt_vit3;
+    private VideoView mVideoView;
+    private ProgressBar pb;
+    private FrameLayout fl_controller;
+    private TextView downloadRateView;
+    private TextView loadRateView;
+    private Uri uri;
+    boolean isPortrait = false;
+    private long mPosition = 0;
+    private String path1 = "rtmp://58.200.131.2:1935/livetv/hunantv";
+    private String path2 = "rtmp://58.200.131.2:1935/livetv/hunantv";
+    private String path3 = "rtmp://58.200.131.2:1935/livetv/hunantv";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vitamio);
+//        Vitamio.initialize(this);
+        initView();
+        initEvent();
+        initData();
+    }
+
+    private void initView() {
+        bt_vit1 = findViewById(R.id.btn_play1);
+        bt_vit2 = findViewById(R.id.btn_play2);
+        bt_vit3 = findViewById(R.id.btn_play3);
+        mVideoView = findViewById(R.id.buffer);
+        fl_controller= findViewById(R.id.fl_controller);
+//        pb = findViewById(R.id.probar);
+//        downloadRateView = findViewById(R.id.download_rate);
+//        loadRateView = findViewById(R.id.load_rate);
+    }
+
+    private void initEvent() {
+        bt_vit1.setOnClickListener(this);
+        bt_vit2.setOnClickListener(this);
+        bt_vit3.setOnClickListener(this);
+    }
+
+    private void initData() {
+        initVideo(path1);
     }
 
     @Override
@@ -29,4 +92,174 @@ public class VitamioActivity extends BaseActivity {
         Intent intent = new Intent(context, VitamioActivity.class);
         context.startActivity(intent);
     }
+
+    private void initVideo(String path){
+        if (path == "") {
+            // Tell the user to provide a media file URL/path.
+            return;
+        } else {
+            /*
+             * Alternatively,for streaming media you can use
+             * mVideoView.setVideoURI(Uri.parse(URLstring));
+             */
+            uri = Uri.parse(path);
+            mVideoView.setVideoURI(uri);
+            mVideoView.start();
+//            MediaController mc = new MediaController(this);
+////            mc.setOnControllerClick(new MediaController.OnControllerClick() {
+////                @Override
+////                public void OnClick(int type) {
+//                    //type 0 全屏。type1 分享
+////                    if (type == 0) {
+//                        if (isPortrait) {
+//                            LinearLayout.LayoutParams fl_lp = new LinearLayout.LayoutParams(
+//                                    getHeightPixel(VitamioActivity.this),
+//                                    getWidthPixel(VitamioActivity.this) - getStatusBarHeight(VitamioActivity.this)
+//                            );
+//
+//                            fl_controller.setLayoutParams(fl_lp);
+//                            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+//
+//                            mVideoView.setVideoLayout(VideoView.VIDEO_LAYOUT_SCALE, 0);
+//                            isPortrait = false;
+//                        } else {
+//                            LinearLayout.LayoutParams fl_lp = new LinearLayout.LayoutParams(
+//                                    LinearLayout.LayoutParams.MATCH_PARENT,
+//                                    DensityUtil.dp2px(VitamioActivity.this, 260)
+//                            );
+//
+//                            fl_controller.setLayoutParams(fl_lp);
+//
+//                            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//                            isPortrait = true;
+//                        }
+
+//                    }
+
+//                }
+//            });
+//            mVideoView.setMediaController(mc);
+//            mc.setVisibility(View.GONE);
+//            //  mVideoView.setMediaController(new MediaController(this));
+//            mVideoView.requestFocus();
+//            mVideoView.setOnInfoListener(this);
+//            mVideoView.setVideoQuality(MediaPlayer.VIDEOQUALITY_HIGH);//设置播放画质 高画质
+//            mVideoView.setOnBufferingUpdateListener(this);
+//            mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+//                @Override
+//                public void onPrepared(MediaPlayer mediaPlayer) {
+//                    // optional need Vitamio 4.0
+//                    mediaPlayer.setPlaybackSpeed(1.0f);
+////                    mVideoView.start();
+//                }
+//            });
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_play1:
+                initVideo(path1);
+                break;
+            case R.id.btn_play2:
+                initVideo(path2);
+                break;
+            case R.id.btn_play3:
+                initVideo(path3);
+                break;
+        }
+    }
+    @Override
+    public boolean onInfo(MediaPlayer mp, int what, int extra) {
+
+        switch (what) {
+            case MediaPlayer.MEDIA_INFO_BUFFERING_START:
+                if (mVideoView.isPlaying()) {
+                    mVideoView.pause();
+                    pb.setVisibility(View.VISIBLE);
+                    downloadRateView.setText("");
+                    loadRateView.setText("");
+                    downloadRateView.setVisibility(View.VISIBLE);
+                    loadRateView.setVisibility(View.VISIBLE);
+
+                }
+                break;
+            case MediaPlayer.MEDIA_INFO_BUFFERING_END:
+                mVideoView.start();
+                pb.setVisibility(View.GONE);
+                downloadRateView.setVisibility(View.GONE);
+                loadRateView.setVisibility(View.GONE);
+                break;
+            case MediaPlayer.MEDIA_INFO_DOWNLOAD_RATE_CHANGED:
+                downloadRateView.setText("" + extra + "kb/s" + "  ");
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==KeyEvent.KEYCODE_BACK){
+            if(!isPortrait){
+                LinearLayout.LayoutParams fl_lp=new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        DensityUtil.dp2px(VitamioActivity.this, 260)
+                );
+                fl_controller.setLayoutParams(fl_lp);
+
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                isPortrait=true;
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        loadRateView.setText(percent + "%");
+    }
+
+    @Override
+    protected void onPause() {
+        mPosition = mVideoView.getCurrentPosition();
+        mVideoView.stopPlayback();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        if (mPosition > 0) {
+            mVideoView.seekTo(mPosition);
+            mPosition = 0;
+        }
+        super.onResume();
+//        mVideoView.start();
+
+
+    }
+
+
+    public  int getHeightPixel(Activity activity)
+    {
+        DisplayMetrics localDisplayMetrics = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(localDisplayMetrics);
+        return localDisplayMetrics.heightPixels;
+    }
+    public  int getWidthPixel(Activity activity)
+    {
+        DisplayMetrics localDisplayMetrics = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(localDisplayMetrics);
+        return localDisplayMetrics.widthPixels;
+    }
+    public  int getStatusBarHeight(Activity activity){
+        Rect frame = new Rect();
+        activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+
+        int statusBarHeight = frame.top;
+        return statusBarHeight;
+    }
+
 }
--- app/src/main/java/com/example/projectdemo/permission/ActivityPermission.java
+++ app/src/main/java/com/example/projectdemo/permission/ActivityPermission.java
@@ -6,15 +6,22 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.Toast;
 
 import com.example.projectdemo.R;
 import com.example.projectdemo.view.paomadeng.RunHorseLampActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActivityPermission extends AppCompatActivity {
 
@@ -22,15 +29,14 @@ public class ActivityPermission extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission);
-
-        cameraCheck();
-        albumCheck();
+        initPermission();
+//        cameraCheck();
     }
 
     /**
      * 相机权限检查
      */
-    private void cameraCheck() {
+    /*private void cameraCheck() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(ActivityPermission.this,
                     Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -41,25 +47,7 @@ public class ActivityPermission extends AppCompatActivity {
                 // 获取权限后需执行的操作
             }
         }
-    }
-
-    /**
-     * 相册检查权限
-     */
-    private void albumCheck() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(ActivityPermission.this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(ActivityPermission.this,
-                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                        2);
-            } else {
-
-                // 获取权限后需执行的操作
-
-            }
-        }
-    }
+    }*/
 
     /**
      * 权限选择回调
@@ -67,7 +55,7 @@ public class ActivityPermission extends AppCompatActivity {
      * @param permissions
      * @param grantResults
      */
-    @Override
+    /*@Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case 1:
@@ -79,18 +67,102 @@ public class ActivityPermission extends AppCompatActivity {
                     Toast.makeText(this, "很遗憾！您拒绝了访问相机权限。", Toast.LENGTH_SHORT).show();
                 }
                 break;
-            case 2:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // 获取权限后需执行的操作
-
-                } else {
-                    Toast.makeText(this, "很遗憾！您拒绝了访问相册权限。", Toast.LENGTH_SHORT).show();
-                }
-                break;
             default:
                 break;
         }
+    }*/
+
+    /**
+     * 批量申请运行时权限
+     */
+    //1、首先声明一个数组permissions，将所有需要申请的权限都放在里面
+    String[] permissions = new String[]{Manifest.permission.CAMERA,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET};
+    //2、创建一个mPermissionList，逐个判断哪些权限未授权，将未授权的权限存储到mPermissionList中
+    List<String> mPermissionList = new ArrayList<>();
+    //3、声明一个请求码，在请求权限的回调方法onRequestPermissionsResult中需要判断使用
+    private final int mRequestCode = 100;//权限请求码
+    //4、权限判断和申请
+    private void initPermission(){
+        mPermissionList.clear();//清空已经允许的没有通过的权限
+        //逐个判断是否还有未通过的权限
+        for (int i = 0;i<permissions.length;i++){
+            if (ContextCompat.checkSelfPermission(this,permissions[i])!=
+                    PackageManager.PERMISSION_GRANTED){
+                mPermissionList.add(permissions[i]);//添加还未授予的权限到mPermissionList中
+            }
+        }
+        //申请权限
+        if (mPermissionList.size()>0){//有权限没有通过，需要申请
+            ActivityCompat.requestPermissions(this,permissions,mRequestCode);
+        }else {
+            //权限已经都通过了，可以将程序继续打开了
+        }
+    }
+    /**
+     * 5.请求权限后回调的方法
+     * @param requestCode 是我们自己定义的权限请求码
+     * @param permissions 是我们请求的权限名称数组
+     * @param grantResults 是我们在弹出页面后是否允许权限的标识数组，数组的长度对应的是权限
+     *                     名称数组的长度，数组的数据0表示允许权限，-1表示我们点击了禁止权限
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean hasPermissionDismiss = false;//有权限没有通过
+        if (mRequestCode==requestCode){
+            for (int i=0;i<grantResults.length;i++){
+                if (grantResults[i]==-1){
+                    hasPermissionDismiss=true;
+                    break;
+                }
+            }
+        }
+        if (hasPermissionDismiss){//如果有没有被允许的权限
+            showPermissionDialog();
+        }else {
+            //权限已经都通过了，可以将程序继续打开了
+        }
+    }
+    /**
+     *  6.不再提示权限时的展示对话框
+     */
+    AlertDialog mPermissionDialog;
+    String mPackName = "com.example.projectdemo";
+
+    private void showPermissionDialog() {
+        if (mPermissionDialog == null) {
+            mPermissionDialog = new AlertDialog.Builder(this)
+                    .setMessage("已禁用权限，请手动授予")
+                    .setPositiveButton("设置", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            cancelPermissionDialog();
+
+                            Uri packageURI = Uri.parse("package:" + mPackName);
+                            Intent intent = new Intent(Settings.
+                                    ACTION_APPLICATION_DETAILS_SETTINGS, packageURI);
+                            startActivity(intent);
+                        }
+                    })
+                    .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //关闭页面或者做其他操作
+                            cancelPermissionDialog();
+                            ActivityPermission.this.finish();
+                        }
+                    })
+                    .create();
+        }
+        mPermissionDialog.show();
+    }
+    //7、关闭对话框
+    private void cancelPermissionDialog() {
+        mPermissionDialog.cancel();
     }
 
     public static void actionStart(Context context) {
